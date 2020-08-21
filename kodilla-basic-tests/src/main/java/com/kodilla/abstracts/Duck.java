8751b725c9e397a26e42e423bb9271f42599333b
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-08-21 14:02:50 +0200

    done

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/Duck.java
@@ -0,0 +1,22 @@
+package com.kodilla.abstracts;
+
+public class Duck extends Animal {
+
+    public Duck() {
+        super(2);
+    }
+
+    @Override
+    public void giveVoice() {
+        System.out.println("Quack");
+    }
+
+   /* public Duck(int numberOfLegs) {
+        super(numberOfLegs);
+    }
+
+    @Override
+    public void giveVoice() {
+        System.out.println("Quack");
+    }*/
+}
