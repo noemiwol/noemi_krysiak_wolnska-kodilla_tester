96af794fe9e64b7619a861578949c59b147cab96
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-08-21 14:53:56 +0200

    Added packed basic_assertion Created class Application, Calculator, ResultChecker

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+
+
+    public int addition(int a, int b){
+        return a + b;
+    }
+
+    public int subtraction (int c, int d){
+        return c - d;
+
+    }
+
+    public int exponentiation (int e){
+        return e * e;
+    }
+}
