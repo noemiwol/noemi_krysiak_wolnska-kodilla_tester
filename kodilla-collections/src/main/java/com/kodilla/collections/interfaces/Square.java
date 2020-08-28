8b14292f9ab9c3833ac531928825676483e4a3d5
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-08-28 14:55:57 +0200

    done

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Square.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Square.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.interfaces;
 
+import java.util.Objects;
+
 public class Square implements Shape {
     private double widht;
 
@@ -15,4 +17,24 @@ public class Square implements Shape {
         return 4 * widht;
     }
 
+    @Override
+    public String toString() {
+        return "Square{" +
+                "widht=" + widht +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Square)) return false;
+        Square square = (Square) o;
+        return Double.compare( square.widht, widht ) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( widht );
+    }
 }
+
--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.interfaces;
 
+import java.util.Objects;
+
 public class Triangle  implements  Shape{
 
     private  double width;
@@ -12,6 +14,8 @@ public class Triangle  implements  Shape{
         this.width = width;
         this.height = height;
         this.hypotenuse = hypotenuse;
+
+
     }
     @Override
     public double getArea() {
@@ -21,5 +25,29 @@ public class Triangle  implements  Shape{
     @Override
     public double getPerimeter() {
         return width + height + hypotenuse;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                ", hypotenuse=" + hypotenuse +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Triangle)) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare( triangle.width, width ) == 0 &&
+                Double.compare( triangle.height, height ) == 0 &&
+                Double.compare( triangle.hypotenuse, hypotenuse ) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( width, height, hypotenuse );
     }
 }
