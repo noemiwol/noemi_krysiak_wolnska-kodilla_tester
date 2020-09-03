d90e3857f98993af83f7592da0fce0a38db6d267
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-09-03 13:34:35 +0200

    Done

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/homework/School.java
@@ -0,0 +1,61 @@
+package com.kodilla.collections.adv.maps.complex.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class School {
+    private String schoolName;
+    private int numberOfStudents;
+
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof School)) return false;
+        School school = (School) o;
+        return Objects.equals( schoolName, school.schoolName ) &&
+                Objects.equals( numberOfStudents, school.numberOfStudents );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( schoolName, numberOfStudents );
+    }
+
+
+
+    public School(String schoolName, int numberOfStudents) {
+        this.schoolName = schoolName;
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public School(Integer... students){
+        for (Integer student: students)
+            this.students.add( student );
+    }
+
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "students=" + students +
+                '}';
+    }
+
+    List<Integer>students = new ArrayList<>();
+
+    public School(int... students){
+        for (int student: students)
+            this.students.add( student );
+    }
+
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/homework/SchoolDirectory.java
@@ -0,0 +1,29 @@
+package com.kodilla.collections.adv.maps.complex.homework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolDirectory {
+    public static void main(String[] args) {
+        Map<Principal, School>listOfPrincipal = new HashMap<>();
+        Principal jozef = new Principal( "Józef", "Kowalski" );
+        Principal zofia = new Principal( "Zofia", "Przech" );
+        Principal tomasz = new Principal( "Tomasz", "Wilk" );
+
+        School podstawowka = new School( "Teb", 400);
+        School liceum = new School( "Żak", 120);
+        School technikum = new School( "Cosinus", 80);
+
+        listOfPrincipal.put( jozef,podstawowka );
+        listOfPrincipal.put( zofia, liceum);
+        listOfPrincipal.put( tomasz, technikum );
+
+        for (Map.Entry<Principal, School>principalSchoolEntry: listOfPrincipal.entrySet()){
+            System.out.println(principalSchoolEntry.getKey().getFirstname() + " "+principalSchoolEntry.getKey().getLastname() + " dane dotyczące szkoły, którą zarządza " +
+                    principalSchoolEntry.getValue().getSchoolName() + " " +principalSchoolEntry.getValue().getNumberOfStudents());
+        }
+
+
+
+    }
+}
