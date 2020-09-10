db18e649a47565ec7996507f4273668fb2f230da
Noemi Krysiak - Wolanska <noemi.krysiak@gmail.com>
2020-09-10 19:02:57 +0200

    Done

---
+++ kodilla-stream/src/main/java/homework/TaskManager.java
@@ -0,0 +1,19 @@
+package homework;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        List<Task> taskList = TaskRepository.getTasks()
+                .stream()
+                .filter(task -> task.getDeadline().isAfter(LocalDate.now()))
+                .collect(Collectors.toList());
+        System.out.println(taskList);
+
+
+
+
+    }
+}
