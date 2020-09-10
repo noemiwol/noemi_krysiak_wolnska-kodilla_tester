48bb99c2c175a1402af03b1a0982fe25dac08e7c
Noemi Krysiak - Wolanska <noemi.krysiak@gmail.com>
2020-09-10 16:58:41 +0200

    Done

---
+++ kodilla-stream/src/main/java/UsersManager.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
+    public static void main(String[] args) {
+        List<String> username = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getGroup().equals("Basic"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+        System.out.println(username);
+    }
+
+    public static String getUserName(User user){
+        return user.getUsername();
+    }
+}
