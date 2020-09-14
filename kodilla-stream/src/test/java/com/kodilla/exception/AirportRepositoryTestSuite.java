97fe1f1b39923b718ef154d431e366335fc56ad7
Noemi Krysiak - Wolanska <noemi.krysiak@gmail.com>
2020-09-14 17:57:19 +0200

    Done

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/AirportRepositoryTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.Assert.*;
+
+public class AirportRepositoryTestSuite {
+
+   @Test
+    public void testIsAirportInUse() throws AirportNotFoundException {
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        boolean isWarsawInUse = airportRepository.isAirportInUse("Warsaw");
+        //then
+        assertTrue(isWarsawInUse);
+    }
+
+    @Test
+    public void testInAirportInUse_withException() throws AirportNotFoundException{
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+
+        //when & then
+        Assertions.assertThrows(AirportNotFoundException.class,
+        () ->
+        {
+            airportRepository.isAirportInUse("Vienna");
+        });
+    }
+
+
+}
