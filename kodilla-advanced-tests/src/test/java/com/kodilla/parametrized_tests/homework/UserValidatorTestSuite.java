cd19ceea7074fcd402d8e3fe805d9d06c09e7e9b
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-09-17 09:46:23 +0200

    Done

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -1,8 +1,7 @@
 package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,17 +11,24 @@ class UserValidatorTestSuite {
 
     @ParameterizedTest
     @CsvSource(value = {"abc,true", "BGFS,true", "123,true","_,false","-,false","aa,false","Ba,false","noemi_wol@1K.-KI,false"})
-   //@ValueSource(strings = {"abc", "BGFS", "123","_","-","aa","Ba","noemi_wol@1K.-KI"})
-    public void shouldReturnTrueIfValidateUsernameCorrect(String username, Boolean expectedResult){
+    public void shouldReturnTrueIfValidateUsername(String username, Boolean expectedResult){
         boolean result = userValidator.validateUsername(username);
         assertEquals(expectedResult, result);
     }
 
-    /*@ParameterizedTest
-    @ValueSource(strings = {"abc", "BGFS", "123","_","-","aa","Ba","noemi_wol@1K.-KI"})
-    public void shouldReturnTrueIfValidateEmailCorrect(String username){
-        boolean result = userValidator.validateUsername(username);
-        assertTrue(result);
-    }*/
+    @ParameterizedTest
+    @EmptySource
+    public void shouldReturnTrueIfStringEmailIsEmptyValidateEmail(String email) {
+    assertTrue(userValidator.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value ={"jan.kowalski@op.pl, true", "o@o.p, true", "sw78_54@sklep.com, true", "o3_O.pl, false",
+            "janKowalskiop.pl, false", "fjUIH_12@jkdigjgfugh.figkfjf,false"})
+    public void shouldReturnTrueIfValidateEmail(String email, Boolean expectedResult){
+        boolean result = userValidator.validateEmail(email);
+        assertEquals(expectedResult, result);
+    }
+
 
 }
