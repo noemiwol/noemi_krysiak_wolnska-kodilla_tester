4f5a3bb33728f30869ed6e85d016196ada5456a3
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-09-18 17:20:20 +0200

    Done

--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/ServiceAboutWeatherHazardsTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/ServiceAboutWeatherHazardsTest.java
@@ -1,63 +1,52 @@
-package com.kodilla.mockito.homework;
+        package com.kodilla.mockito.homework;
 
-import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
+        import org.junit.jupiter.api.Test;
+        import org.mockito.Mockito;
 
-    class ServiceAboutWeatherHazardsTest{
-        ServiceAboutWeatherHazards serviceAboutWeatherHazards = new ServiceAboutWeatherHazards();
-        Client client = mock(Client.class);
-        Notification notification = mock(Notification.class);
+        import static org.mockito.Mockito.*;
+
+        class ServiceAboutWeatherHazardsTest{
         @Test
         public void addLocation() {
-            serviceAboutWeatherHazards.addLocation("Warszawa",client);
-            verify(client);
+            ServiceAboutWeatherHazards serviceAboutWeatherHazards = new ServiceAboutWeatherHazards();
+            Client client = mock(Client.class);
+            Client client1 = mock(Client.class);
+            serviceAboutWeatherHazards.addLocation("Warszawa", client1);
+            Mockito.verify(client);
         }
 
         @Test
-         public void oneClientOneLocationSendNotification() {
-             Client client1 = mock(Client.class);
-             serviceAboutWeatherHazards.addLocation("Warszawa", client1);
-
-             serviceAboutWeatherHazards.sendNotification(notification);
-             verify(client1, times(1)).getReceive(notification);
-         }
-        @Test
         public void oneClientTreeLocationSendNotification() {
+            ServiceAboutWeatherHazards serviceAboutWeatherHazards = new ServiceAboutWeatherHazards();
+            Notification notification = mock(Notification.class);
             Client client1 = mock(Client.class);
             serviceAboutWeatherHazards.addLocation("Warszawa", client1);
             serviceAboutWeatherHazards.addLocation("Kraków", client1);
             serviceAboutWeatherHazards.addLocation("Poznań", client1);
 
             serviceAboutWeatherHazards.sendNotification(notification);
-            verify(client1, times(3)).getReceive(notification);
+            Mockito.verify(client1, times(3)).getReceive(notification);
         }
 
-   @Test
-    void treeClientsOneLocationSendNotification() {
-        Client client1 = mock(Client.class);
-        Client client2 = mock(Client.class);
-        Client client3 = mock(Client.class);
-        serviceAboutWeatherHazards.addLocation("Warszawa",client1);
-        serviceAboutWeatherHazards.addLocation("Warszawa",client2);
-        serviceAboutWeatherHazards.addLocation("Warszawa",client3);
-
-        serviceAboutWeatherHazards.sendNotification(notification);
-        verify(client1, times(1)).getReceive(notification);
-        verify(client2, times(1)).getReceive(notification);
-        verify(client3, times(1)).getReceive(notification);
-    }
 
         @Test
         void removeOneSubscriberLocation() {
+            ServiceAboutWeatherHazards serviceAboutWeatherHazards = new ServiceAboutWeatherHazards();
+            Client client = mock(Client.class);
+            Notification notification = mock(Notification.class);
+            Client client1 = mock(Client.class);
+            serviceAboutWeatherHazards.addLocation("Warszawa", client1);
             serviceAboutWeatherHazards.addLocation("Warszawa",client);
             serviceAboutWeatherHazards.removeOneSubscriberLocation("Warszawa");
 
             serviceAboutWeatherHazards.sendNotification(notification);
-            verify(client, never()).getReceive(notification);
+            Mockito.verify(client, never()).getReceive(notification);
         }
 
         @Test
         void treeClientOneLocationRemoveAllSubscriberLocation() {
+            ServiceAboutWeatherHazards serviceAboutWeatherHazards = new ServiceAboutWeatherHazards();
+            Notification notification = mock(Notification.class);
             Client client1 = mock(Client.class);
             Client client2 = mock(Client.class);
             Client client3 = mock(Client.class);
@@ -69,12 +58,14 @@ import static org.mockito.Mockito.*;
             serviceAboutWeatherHazards.removeAllSubscriberLocation("Warszawa",client3);
 
             serviceAboutWeatherHazards.sendNotification(notification);
-            verify(client1, never()).getReceive(notification);
-            verify(client2, never()).getReceive(notification);
-            verify(client3, never()).getReceive(notification);
+            Mockito.verify(client1, never()).getReceive(notification);
+            Mockito.verify(client2, never()).getReceive(notification);
+            Mockito.verify(client3, never()).getReceive(notification);
         }
         @Test
         public void oneClientTreeLocationRemoveAllSubscriberLocation() {
+            ServiceAboutWeatherHazards serviceAboutWeatherHazards = new ServiceAboutWeatherHazards();
+            Notification notification = mock(Notification.class);
             Client client1 = mock(Client.class);
             serviceAboutWeatherHazards.addLocation("Warszawa", client1);
             serviceAboutWeatherHazards.addLocation("Kraków", client1);
@@ -84,7 +75,6 @@ import static org.mockito.Mockito.*;
             serviceAboutWeatherHazards.removeAllSubscriberLocation("Poznań",client1);;
 
             serviceAboutWeatherHazards.sendNotification(notification);
-            verify(client1, never()).getReceive(notification);
+            Mockito.verify(client1, never()).getReceive(notification);
         }
-
-    }
+        }
