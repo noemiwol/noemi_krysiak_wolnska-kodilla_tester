b9042eeeee4d5fdff30588f9431744d3c849f907
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-09-30 16:09:56 +0200

    Added class ShippingCenter

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class ShippingCenter {
+
+    @Autowired
+    private final DeliveryService deliveryService;
+    private final NotificationService notificationService;
+
+    public ShippingCenter(DeliveryService deliveryService, NotificationService notificationService) {
+        this.deliveryService = deliveryService;
+        this.notificationService = notificationService;
+    }
+
+
+
+       public String sendPackage(String address, double weight) {
+        if (deliveryService.deliverPackage(address, weight)) {
+            return notificationService.success(address);
+        }
+        return notificationService.fail(address);
+    }
+
+}
