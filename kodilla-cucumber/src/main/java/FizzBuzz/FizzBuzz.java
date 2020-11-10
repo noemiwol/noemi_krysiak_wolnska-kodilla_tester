f48d1ae3aa4e2fb57a96aa7b01aa2c3097e392ac
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2020-11-10 12:14:53 +0100

    Done

---
+++ kodilla-cucumber/src/main/java/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+package FizzBuzz;
+
+public class FizzBuzz {
+    public String checkIsItFizzBuzz(int number){
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+        else if(number % 5 == 0){
+            return "Buzz";
+        }
+        else if (number % 3 == 0){
+            return "Fizz";
+        }
+        return "None";
+    }
+}
--- kodilla-cucumber/src/test/java/FizzBuzz/FizzBuzzTestRunner.java
+++ kodilla-cucumber/src/test/java/FizzBuzz/FizzBuzzTestRunner.java
@@ -1,11 +1,12 @@
-package seasonchecker;
+package FizzBuzz;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/seasonchecker/is_it_summer.feature"}
+        features = {"src/test/resources/FizzBuzz/is_it_Fizz_or_Buzz_or_FizzBuzz.feature"}
 )
-public class TestRunner {
+public class FizzBuzzTestRunner {
 }
